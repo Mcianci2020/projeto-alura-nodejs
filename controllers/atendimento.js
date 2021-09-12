1892e33d7fff0a78cb006940f25168905dcd1b69
Marcos Cianci <marcos.cianci@gmail.com>
2021-09-12 16:22:29 -0300

    Remove console.log insert res.status HTTP Status

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -3,10 +3,9 @@ const Atendimento = require('../models/atendimentos')
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Voce esta na rota atendimentos e esta realizando um GET'))
 
-    app.post('/atendimentos', (req,res) => {
+    app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
         
-        Atendimento.adciona(atendimento)
-        res.send('Atendimento POST')   
+        Atendimento.adciona(atendimento, res)
     })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,11 @@
+
 const moment = require('moment')
 const conexao = require('../database/conexao')
 
 
 class Atendimentos {
 
-    adciona(atendimento) {
+    adciona(atendimento, res) {
 
         const dataCreate = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
@@ -14,13 +15,13 @@ class Atendimentos {
 
         conexao.query(sql, atendimentoDate, (erro, resultados) => {
 
-            if(erro){
-                console.log(erro) 
-            } else{
-                console.log(resultados)
+            if(erro){ 
+                res.status(400).json(erro) 
+            } 
+            else { 
+                res.status(201).json(resultados) 
             }
         })
-
     }        
 }
 
