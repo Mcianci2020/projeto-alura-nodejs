0129fcd80de4875b023c2cbcd032ff51217bbd6b
Marcos Cianci <marcos.cianci@gmail.com>
2021-09-12 15:47:43 -0300

    New deal data in database

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 const conexao = require('../database/conexao')
 
 
@@ -5,9 +6,13 @@ class Atendimentos {
 
     adciona(atendimento) {
 
+        const dataCreate = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDate = { ...atendimento, dataCreate, data}
+
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDate, (erro, resultados) => {
 
             if(erro){
                 console.log(erro) 
