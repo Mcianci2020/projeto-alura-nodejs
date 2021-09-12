438ff48dc66055923ea4f9bcfd81481ce48e25d1
Marcos Cianci <marcos.cianci@gmail.com>
2021-09-12 16:54:05 -0300

    Create validate table cliente and date

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -9,19 +9,45 @@ class Atendimentos {
 
         const dataCreate = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDate = { ...atendimento, dataCreate, data}
-
-        const sql = 'INSERT INTO Atendimentos SET ?'
-
-        conexao.query(sql, atendimentoDate, (erro, resultados) => {
-
-            if(erro){ 
-                res.status(400).json(erro) 
-            } 
-            else { 
-                res.status(201).json(resultados) 
+        
+        const dataValidate = moment(data).isSameOrAfter(dataCreate)
+        const ClientValidate = atendimento.cliente.length >= 5
+        
+        const validates = [
+            {
+                nome: 'data',
+                valido: dataValidate,
+                mensagem: 'Data dever ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: ClientValidate,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-        })
+        ]
+        
+        const error = validates.filter(campo => !campo.valido)
+        const ExistError = error.length
+
+        if(ExistError){
+            res.status(400).json(error)
+        } else{
+
+    
+            const atendimentoDate = { ...atendimento, dataCreate, data}
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDate, (erro, resultados) => {
+
+                if(erro){ 
+                    res.status(400).json(erro) 
+                } 
+                else { 
+                    res.status(201).json(resultados) 
+                }
+            })
+        }    
     }        
 }
 
