ae6da9e35f735fabf68ce9fc55ae0f813754db2a
Marcos Cianci <marcos.cianci@gmail.com>
2021-09-18 15:45:51 -0300

    Create route '/pets'

---
+++ controllers/pets.js
@@ -0,0 +1,13 @@
+const Pet = require ('../models/pets')
+
+
+module.exports = app => {
+
+    app.post('/pet', (req, res) => {
+        const pet = req.body
+
+        Pet.adciona(pet, res)
+        
+    })
+
+}
