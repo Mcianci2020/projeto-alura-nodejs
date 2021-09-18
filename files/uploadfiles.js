3b6103ed8911b8ba71d5b13a2441ccf93917885d
Marcos Cianci <marcos.cianci@gmail.com>
2021-09-18 19:28:07 -0300

    Manupile images

--- files/uploadfiles.js
+++ files/uploadfiles.js
@@ -1,7 +1,31 @@
 const fs = require('fs')
+const path = require('path')
+
+module.exports = (caminho, nameFile, callbackCreateImage) => {
+
+    const typeValidate = ['jpeg','png','jpeg']
+    const type = path.extname(caminho)
+    const validate = typeValidate.indexOf(type.substring(1)) !== -1
+
+    if(validate){
+
+        const newSource = `./assets/images/${nameFile}${type}`    
+        
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(newSource))
+            .on('finish', () => callbackCreateImage(false,newSource))
+        
+    }
+    else {
+    
+        const erro = "Tipo invalido!!!"
+        console.log('Erro tipo invalido!!!')
+        callbackCreateImage(erro)
+
+    }
+}
 
 
-fs.createReadStream("./assets/pitbull.jpeg")
-    .pipe(fs.createWriteStream('./assets/pitbull-stream.jpeg'))
-    .on('finish', () => console.log('Imagem escrita com sucesso!!!'))
+
+
 
--- models/pets.js
+++ models/pets.js
@@ -1,21 +1,31 @@
-const conexao = require ('../database/conexao')
+const conexao = require('../database/conexao')
+const uploadfile = require('../files/uploadfiles')
 
 class Pet {
 
     adciona(pet, res) {
-
         const query = 'INSERT INTO Pets SET ?'
         
-        conexao.query(query, pet, erro => {
+        uploadfile(pet.imagem, pet.nome, (erro, newSource) => {
 
-                if(erro){
-                    res.status(400).json(erro)
-                }else {
-                    res.status(200).json(pet)
-                }   
+            if(erro){
+                res.status(400).json(erro)
+
+            }else{
+
+                const newPet = { nome: pet.nome, imagem: newSource }
+
+                conexao.query(query, newPet, erro => {
+
+                    if(erro){
+                        res.status(400).json(erro)
+                    }else {
+                        res.status(200).json(newPet)
+                    }
+                })
+            }   
         })
     }
-
 }
 
 module.exports = new Pet
